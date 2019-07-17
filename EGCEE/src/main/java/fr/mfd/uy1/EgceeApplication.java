09cf958b18f74746dcc6bc455987700abdc6919f
Marius TENE <tefuncowm@gmail.com>
2019-07-17 03:09:22 +0100

    creation du projet et ajout des dépendances au pom.xml

---
+++ EGCEE/src/main/java/fr/mfd/uy1/EgceeApplication.java
@@ -0,0 +1,13 @@
+package fr.mfd.uy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgceeApplication.class, args);
+	}
+
+}
---
+++ EGCEE/src/test/java/fr/mfd/uy1/EgceeApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.mfd.uy1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EgceeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
